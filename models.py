3819d5cad7c8ff76204bcaf2d63963c44796fe46
windows <windows@ubuntu.ubuntu-domain>
2015-04-07 21:17:01 +0200

    eliminado

--- models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Veraneo(models.Model):
-    sitio = models.CharField(max_length=32)
-    precio = models.PositiveIntegerField()
-    duracion = models.PositiveSmallIntegerField()
